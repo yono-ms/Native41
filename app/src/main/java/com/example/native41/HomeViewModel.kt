cbaf41b4efb842cf01aeeaf85c96b3969fbb4211
yono-ms <yono.ms@outlook.com>
2020-10-20 22:33:39 +0900

    repos api. (#2)
    Co-authored-by: yono.ap <yono.ap@icloud.com>

--- app/src/main/java/com/example/native41/HomeViewModel.kt
+++ app/src/main/java/com/example/native41/HomeViewModel.kt
@@ -1,8 +1,10 @@
 package com.example.native41
 
 import androidx.lifecycle.viewModelScope
+import com.example.native41.network.RepoModel
 import com.example.native41.network.ServerAPI
 import kotlinx.coroutines.launch
+import kotlinx.serialization.builtins.ListSerializer
 
 class HomeViewModel : BaseViewModel() {
     fun initialize() {
@@ -10,10 +12,24 @@ class HomeViewModel : BaseViewModel() {
         viewModelScope.launch {
             kotlin.runCatching {
                 ServerAPI.getUsers("google")
-            }.onSuccess {
-                logger.debug("$it")
+            }.onSuccess { userModel ->
+                logger.debug("$userModel")
+                kotlin.runCatching {
+                    ServerAPI.getDecode(
+                        userModel.reposUrl,
+                        ListSerializer(RepoModel.serializer())
+                    )
+                }.onSuccess { repos ->
+                    logger.debug("$repos")
+                    repos.forEach {
+                        logger.debug("name=${it.name}")
+                        logger.debug("updatedAt=${it.updatedAt}")
+                    }
+                }.onFailure {
+                    logger.error("initialize repos", it)
+                }
             }.onFailure {
-                logger.error("initialize", it)
+                logger.error("initialize users", it)
             }
         }
     }
--- app/src/main/java/com/example/native41/network/ServerAPI.kt
+++ app/src/main/java/com/example/native41/network/ServerAPI.kt
@@ -2,6 +2,7 @@ package com.example.native41.network
 
 import com.github.kittinunf.fuel.Fuel
 import com.github.kittinunf.fuel.coroutines.awaitStringResponseResult
+import kotlinx.serialization.DeserializationStrategy
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
 import org.slf4j.LoggerFactory
@@ -20,12 +21,21 @@ class ServerAPI {
         }
 
         suspend fun getUsers(login: String): UserModel {
-            val (request, response, result) = Fuel.get("$url_users$login")
+            val text = get("$url_users$login")
+            return json.decodeFromString(text)
+        }
+
+        suspend fun <T> getDecode(urlString: String, deserializer: DeserializationStrategy<T>): T {
+            val text = get(urlString)
+            return json.decodeFromString(deserializer, text)
+        }
+
+        suspend fun get(urlString: String): String {
+            val (request, response, result) = Fuel.get(urlString)
                 .awaitStringResponseResult()
             logger.info("$request")
             logger.info("$response")
-            val text = result.get()
-            return json.decodeFromString(text)
+            return result.get()
         }
     }
 
