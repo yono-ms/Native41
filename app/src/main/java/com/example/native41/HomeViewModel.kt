72c158430b965afd56d6e0e6649135f0ac7404d3
yono-ms <yono.ms@outlook.com>
2020-10-20 21:27:22 +0900

    user api. (#1)
    Co-authored-by: yono.ap <yono.ap@icloud.com>

--- app/src/main/java/com/example/native41/HomeViewModel.kt
+++ app/src/main/java/com/example/native41/HomeViewModel.kt
@@ -1,7 +1,20 @@
 package com.example.native41
 
+import androidx.lifecycle.viewModelScope
+import com.example.native41.network.ServerAPI
+import kotlinx.coroutines.launch
+
 class HomeViewModel : BaseViewModel() {
     fun initialize() {
         logger.info("initialize")
+        viewModelScope.launch {
+            kotlin.runCatching {
+                ServerAPI.getUsers("google")
+            }.onSuccess {
+                logger.debug("$it")
+            }.onFailure {
+                logger.error("initialize", it)
+            }
+        }
     }
 }
